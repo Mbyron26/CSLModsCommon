5c461daf71eade65e8545f7f9443f611d32cf335
Mbyron26 <Michael_byron26@icloud.com>
2025-12-28 11:24:31 +0800

    Remove the CurrentModVersion property from default initialization.

--- CSLModsCommonShared/Setting/ModSettingBase.cs
+++ CSLModsCommonShared/Setting/ModSettingBase.cs
@@ -5,11 +5,11 @@ namespace CSLModsCommon.Setting;
 
 public abstract partial class ModSettingBase : IModSetting {
     public static string DefaultFilePath { get; } = Path.Combine(FileLocationAttribute.DefaultDirectory, "GlobalSetting.json");
-    public ModVersion CurrentModVersion { get; set; } = new();
+    public ModVersion CurrentModVersion { get; set; }
     public string LocaleId { get; set; } = LocalizationManager.UseGameLanguage;
 
     public virtual void SetDefaults() {
-        CurrentModVersion = new();
+        CurrentModVersion = new ModVersion();
         LocaleId = LocalizationManager.UseGameLanguage;
     }
 }
